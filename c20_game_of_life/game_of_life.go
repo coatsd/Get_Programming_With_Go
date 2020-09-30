b1706cc575450a940951bd4febd29fb3ca9598d2
daniel coats <daniel.e.coats@gmail.com>
2020-09-29 22:34:53 -0700

    Fixed a bug in c20/game_of_life.go in neighbors method.

--- c20_game_of_life/game_of_life.go
+++ c20_game_of_life/game_of_life.go
@@ -9,21 +9,21 @@ import (
 type universe [][]bool
 
 // Creates a new instance of a universe - every cell is initialized to false
-func newUniverse(width int, height int) universe {
-	w := make([][]bool, width)
-	for h, _ := range w {
-		w[h] = make([]bool, height)
+func newUniverse(height int, width int) universe {
+	h := make([][]bool, height)
+	for w, _ := range h {
+		h[w] = make([]bool, width)
 	}
-	return w
+	return h
 }
 
 // prints out a universe's 2d array, using 1 for true and 0 for false
 func (u universe) showUniverse() {
 	c0 := "0"
 	c1 := "1"
-	for _, w := range u {
-		for _, h := range w {
-			if h {
+	for _, h := range u {
+		for _, w := range h {
+			if w {
 				fmt.Printf(" %v", c1)	
 			} else {
 				fmt.Printf(" %v", c0)
@@ -37,61 +37,15 @@ func (u universe) showUniverse() {
 func (u universe) calcPopPercent() float64 {
 	var totalCells int
 	var liveCells int
-	for _, w := range u	{
-		totalCells += len(w)
-		for h, _ := range w {
-			if w[h] == true {
+	for _, h := range u	{
+		totalCells += len(h)
+		for w, _ := range h {
+			if h[w] == true {
 				liveCells++
 			}
 		}
 	}
 	return float64(liveCells) / float64(totalCells) * 100
-}
-
-// seeds a universe, with a 25% chance for each cell to become alive
-func (u universe) seedUniverse() {
-	rand.Seed(time.Now().UnixNano())
-	for _, w := range u	{
-		for h, _ := range w {
-			w[h] = rand.Intn(4) == 3
-		}
-	}
-}
-
-// If x or y are out of range, it loops the coordinate to the beginning
-func (u universe) treatCoord(x int, y int) (int, int) {
-	if x >= len(u) {
-		x = x % len(u)
-	} else if x < 0 {
-		x = (x % -len(u)) + len(u)
-	}
-	if y >= len(u[x]) {
-		y = y % len(u[x])
-	} else if y < 0 {
-		y = (y % -len(u[x])) + len(u[x])
-	}
-	return x, y
-}
-
-// tests a cell to see if it is alive (set to true)
-func (u universe) isAlive(x int, y int) bool {
-	return u[x][y]
-}
-
-// calculates how many cells around a coordinate has a living cell
-func (u universe) neighbors(x int, y int) int {
-	var n int = 0
-	for w := -1; w < 2; w++ {
-		for h := -1; h < 2; h++ {
-			if w != 0 || h != 0 {
-				x, y = u.treatCoord(x + w, y + h)
-				if u[x][y] {
-					n++
-				}
-			}
-		}
-	}
-	return n
 }
 
 // prints a universe's state and percentage of living cells
@@ -100,10 +54,57 @@ func (u universe) printUniverseInfo() {
 	fmt.Printf("living cell percentage: %v%%\n", u.calcPopPercent())
 }
 
+// seeds a universe, with a 25% chance for each cell to become alive
+func (u universe) seedUniverse() {
+	rand.Seed(time.Now().UnixNano())
+	for _, h := range u	{
+		for w, _ := range h {
+			h[w] = rand.Intn(4) == 3
+		}
+	}
+}
+
+// If x or y are out of range, it loops the coordinate to the other side
+func (u universe) treatCoord(y int, x int) (int, int) {
+	if y >= len(u) {
+		y = y % len(u)
+	} else if y < 0 {
+		y = (y % -len(u)) + len(u)
+	}
+	if x >= len(u[y]) {
+		x = x % len(u[y])
+	} else if x < 0 {
+		x = (x % -len(u[y])) + len(u[y])
+	}
+	return y, x
+}
+
+// tests a cell to see if it is alive (set to true)
+func (u universe) isAlive(y int, x int) bool {
+	return u[y][x]
+}
+
+// calculates how many cells around a coordinate has a living cell
+func (u universe) neighbors(y int, x int) int {
+	var n int = 0
+	var ty, tx int
+	for h := -1; h < 2; h++ {
+		for w := -1; w < 2; w++ {
+			if h != 0 || w != 0 {
+				ty, tx = u.treatCoord(y + h, x + w)
+				if u[ty][tx] {
+					n++
+				}
+			}
+		}
+	}
+	return n
+}
+
 // calculates if a cell will continue to live, or if a cell will start living
-func (u universe) willLive(x int, y int) bool {
-	var n int = u.neighbors(x, y)
-	if u.isAlive(x, y) {
+func (u universe) willLive(y int, x int) bool {
+	var n int = u.neighbors(y, x)
+	if u.isAlive(y, x) {
 		if n < 2 || n > 3 {
 			return false
 		}
@@ -117,18 +118,18 @@ func (u universe) willLive(x int, y int) bool {
 
 func main() {
 	const (
-		width = 20
 		height = 10
+		width = 10
 	)
 
-	initU := newUniverse(width, height)
+	initU := newUniverse(height, width)
 	initU.seedUniverse()
 	fmt.Printf("Initial Universe:\n")
 	initU.printUniverseInfo()
-	nextU := newUniverse(width, height)
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			nextU[x][y] = initU.willLive(x, y)
+	nextU := newUniverse(height, width)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			nextU[y][x] = initU.willLive(y, x)
 		}
 	}
 	fmt.Printf("\nNext Generation:\n")
