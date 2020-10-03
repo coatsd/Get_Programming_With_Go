42a8b68f62653f6a4d55f943c137648fee64d02e
daniel coats <daniel.e.coats@gmail.com>
2020-10-03 14:54:35 -0700

    edited the c26 turtle to use a method rather than a function.

--- c26_pointers/turtle.go
+++ c26_pointers/turtle.go
@@ -12,7 +12,7 @@ func (t turtle) String() string {
 	return fmt.Sprintf("X: %v, Y: %v", t.x, t.y)
 }
 
-func move(x, y int, t *turtle) {
+func (t *turtle) move(x, y int) {
 	t.x += x
 	t.y += y
 }
@@ -20,6 +20,6 @@ func move(x, y int, t *turtle) {
 func main() {
 	t := turtle{2, 2}
 	fmt.Println(t)
-	move(1, 3, &t)
+	t.move(1, 3)
 	fmt.Println(t)
 }
