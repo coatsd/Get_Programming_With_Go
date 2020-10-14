04c7ba5bd22080ebdc88cba64378eee588a70eaf
coatsd <35239532+coatsd@users.noreply.github.com>
2020-10-14 11:33:32 -0700

    completed c 28 - errors

--- c28_errors/url.go
+++ c28_errors/url.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"os"
 )
 
-func handleError(err URL.Error) {
-
+func handleError(err error) {
+	if e, ok := err.(*url.Error); ok {
+		fmt.Printf("Operation: %v, URL: %v, Error: %v\n", e.Op, e.URL, e.Err)
+	}
+	os.Exit(1)
 }
 
 func main() {
 	const invalidUrl string = "https://a b.com/"
 
-	parsedUrl, err := url.Parse(invalidUrl)
+	_, err := url.Parse(invalidUrl)
 	if err != nil {
 		handleError(err)
 	}
