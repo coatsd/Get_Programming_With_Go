1169c0ee97e7512f6f8c6d1052b8f02b192aa355
coatsd <35239532+coatsd@users.noreply.github.com>
2020-10-28 18:10:02 -0700

    Slight restructuring of c29 to use pointers, so we can check for nils.

--- c29_sudoku/sudoku.go
+++ c29_sudoku/sudoku.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"error"
 	"os"
 )
 
@@ -12,15 +11,15 @@ type sudokuError struct {
 	v int8
 }
 
-func NewError(e string, y,x int, v int8) sudokuError {
-	return sudokuError{err: e, y: y, x: x, v: v,}
+func NewError(e string, y,x int, v int8) *sudokuError {
+	return &sudokuError{err: e, y: y, x: x, v: v,}
 }
 
-func (sg *sudokuError) Error() string {
+func (sg sudokuError) Error() string {
 	if sg.x > -1 {
-		return fmt.Sprintf("Error: %v, Coords: %v, %v, Value: %v", sg.err, sg.x, sg.y, sg.v)
+		return fmt.Sprintf("Error: %v, Coords: %v, %v, Value: %v\n", sg.err, sg.x, sg.y, sg.v)
 	}
-	return fmt.Sprintf("Error: %v", sg.err)
+	return fmt.Sprintf("Error: %v\n", sg.err)
 }
 
 type sudokuGrid struct {
@@ -35,46 +34,50 @@ func NewSudoku(grid [9][9]int8) sudokuGrid {
 			init[h][w] = grid[h][w] == 0
 		}
 	}
-	return sg{initState: init, currState: grid,}
+	return sudokuGrid{initState: init, currState: grid,}
 }
 
-func (sg *sudokuGrid) CanPlace(y,x int, v int8) sudokuError {
+func (sg *sudokuGrid) CanPlace(y,x int, v int8) *sudokuError {
+	var errPt *sudokuError
 	if x >= 0 && x < 9 && y >= 0 && y < 9 {
-		if sg.initState[y][x] {
-			return true, nil
-		} else {
-			return false, NewError("Cannot replace values from the initial puzzle state", y, x, v)
+		if !sg.initState[y][x] {
+			errPt = NewError("Cannot replace values from the initial puzzle state", y, x, v)
 		}
 	} else {
-		return false, NewError("Out of range (x and y values need to be between 0 and 8)", y, x, v)
+		errPt = NewError("Out of range (x and y values need to be between 0 and 8)", y, x, v)
 	}
+	return errPt
 }
 
-func (sg *sudokuGrid) PlaceNum(y,x int, v int8) error {
-	can, err := CanPlace(y, x, v)
-	if can {
+func (sg *sudokuGrid) PlaceNum(y,x int, v int8) *sudokuError {
+	err := sg.CanPlace(y, x, v)
+	if err != nil {
 		sg.currState[y][x] = v
 	}
 	return err
 }
 
-func (sg *sudokuGrid) RemoveNum(y,x int) error {
+func (sg *sudokuGrid) RemoveNum(y,x int) *sudokuError {
+	var errPt *sudokuError
 	if sg.initState[y][x] {
-		sg.currState = 0
+		sg.currState[y][x] = 0
+	} else {
+		errPt = NewError("Cannot replace values from the initial puzzle state", y, x, 0)
 	}
+	return errPt
 }
 
 func HandleError(err error) {
-	if e, ok := err.(sudokuError); ok {
-		fmt.Printf(e)
+	if e, ok := err.(*sudokuError); ok {
+		fmt.Printf(e.Error())
 	} else {
-		fmt.Printf(e)
-		os.exit(1)
+		fmt.Printf(e.Error())
+		os.Exit(1)
 	}
 }
 
 func main() {
-	s := NewSudoku([9][9]int8{
+	sg := NewSudoku([9][9]int8{
 		{5,3,0,0,7,0,0,0,0},
 		{6,0,0,1,9,5,0,0,0},
 		{0,9,8,0,0,0,0,6,0},
@@ -85,4 +88,6 @@ func main() {
 		{0,0,0,4,1,9,0,0,5},
 		{0,0,0,0,8,0,0,7,9},
 	})
+
+	HandleError(sg.CanPlace(9,9,1))
 }
