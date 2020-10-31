60e4d5eafead036d8611f58f3c332abf71987d90
coatsd <35239532+coatsd@users.noreply.github.com>
2020-10-31 09:59:31 -0700

    Started testing. Need to find out why CheckCoord tests are failing

--- c29_sudoku/sudoku.go
+++ c29_sudoku/sudoku.go
@@ -11,6 +11,7 @@ type sudokuError struct {
 	v int8
 }
 
+// Use this when there's an error regarding a specific coordinate or value
 func NewError(e string, y,x int, v int8) *sudokuError {
 	return &sudokuError{err: e, y: y, x: x, v: v,}
 }
@@ -39,14 +40,21 @@ func NewSudoku(grid [9][9]int8) sudokuGrid {
 
 func isValidCoord(y,x int) bool {
 	return x >= 0 && x < 9 && y >= 0 && y < 9
-	
+}
+
+func isValidValue(v int8) bool {
+	return v >= 0 && v < 10
 }
 
 func (sg *sudokuGrid) CanPlace(y,x int, v int8) *sudokuError {
 	var errPt *sudokuError
 	if isValidCoord(y,x) {
-		if !sg.initState[y][x] {
-			errPt = NewError("Cannot replace values from the initial puzzle state", y, x, v)
+		if isValidValue(v) {
+			if !sg.initState[y][x] {
+				errPt = NewError("Cannot replace values from the initial puzzle state", y, x, v)
+			}
+		} else {
+			errPt = NewError("Sudoku values must be between 0 and 9", y, x, v)
 		}
 	} else {
 		errPt = NewError("Out of range (x and y values need to be between 0 and 8)", y, x, v)
@@ -107,7 +115,7 @@ func (sg *sudokuGrid) CheckCoord(y,x int) (bool, error) {
 			}
 		}
 	} else {
-		return false, errors.New(fmt.Sprintf("Index out of range ([%v][%v])", y, x))
+		return false, errors.New(fmt.Sprintf("Index out of range ([%v][%v])\n", y, x))
 	}
 	return true, nil
 }
@@ -116,7 +124,7 @@ func HandleError(err error) {
 	if e, ok := err.(*sudokuError); ok {
 		fmt.Printf(e.Error())
 	} else {
-		fmt.Printf(e.Error())
+		fmt.Printf(err.Error())
 	}
 }
 
@@ -140,18 +148,53 @@ func main() {
 	if err := sg.PlaceNum(9,9,1); err != nil {
 		HandleError(err)
 	} else {
-		fmt.Printf("Something went wrong while testing out of range errors\n")
+		fmt.Println("Something went wrong while testing out of range errors in PlaceNum")
+	}
+
+	fmt.Println("Testing error handling with invalid sudoku puzzle value:")
+	if err := sg.PlaceNum(0,2,1); err != nil {
+		HandleError(err)
+	} else {
+		fmt.Println("Something went wrong while testing invalid sudoku input in PlaceNum")
 	}
 
 	fmt.Println("Testing error handling with attempted init puzzle state overwrites:")
 	if err := sg.PlaceNum(0,0,1); err != nil {
 		HandleError(err)
 	} else {
-		fmt.Printf("Something went wrong while testing init state overwrite errors\n")
+		fmt.Println("Something went wrong while testing init state overwrite errors in PlaceNum")
 	}
 
 	fmt.Println("If the state has changed, something is wrong:")
 	sg.PrintCurrState()
 
-	//fmt.Println("")
+	fmt.Println("Testing the Error handling in CheckCoord method (should return index out of range):")
+	if _, err := sg.CheckCoord(9,9); err != nil {
+		HandleError(err)
+	} else {
+		fmt.Println("Something went wrong while testing out of range errors in CheckCoord")
+	}
+
+	var pFunc = func(p bool, out string) {
+		fmt.Printf("Testing " + out + ": Test ")
+		if p {
+			fmt.Printf("Success\n")
+		} else {
+			fmt.Printf("Fail\n")
+		}
+	}
+	p1, _ := sg.CheckCoord(0,0)
+	pFunc(p1, "if preexisting sudoku values pass CheckCoord")
+	p2, _ := sg.CheckCoord(0,2)
+	pFunc(!p2, "if empty values in puzzle fail CheckCoord")
+	sg.PlaceNum(0,2,7)
+	p3, _ := sg.CheckCoord(0,2)
+	pFunc(!p3, "if wrong value placed in puzzle failed to pass CheckCoord (x axis)")
+	sg.PlaceNum(1,1,6)
+	p4, _ := sg.CheckCoord(0,2)
+	pFunc(!p4, "if wrong value placed in puzzle failed to pass CheckCoord (y axis)")
+	sg.RemoveNum(0,2)
+	sg.PlaceNum(1,1,4)
+	p5, _ := sg.CheckCoord(1,1)
+	pFunc(!p5, "if wrong value placed in puzzle failed to pass CheckCoord (3 x 3)")
 }
