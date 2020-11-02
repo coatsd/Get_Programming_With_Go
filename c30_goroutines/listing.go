c64272661ad85a6c609e557ecc756bc779da2cff
coatsd <35239532+coatsd@users.noreply.github.com>
2020-11-01 20:02:16 -0800

    Did c30 listing 30.5

--- c30_goroutines/listing.go
+++ c30_goroutines/listing.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"time"
 	"flag"
+	"math/rand"
 )
 
 func main() {
-	numPt := flag.Int("v", 2, "Run a particular listing")
+	numPt := flag.Int("v", 2, "Run a particular listing version")
 
 	flag.Parse()
 
@@ -16,8 +17,10 @@ func main() {
 		verOne()
 	case 2:
 		verTwo()
+	case 3:
+		verThree()
 	default:
-		verTwo()
+		verThree()
 	}
 }
 
@@ -58,7 +61,36 @@ func verTwo() {
 	}
 	// This loop waits for the channel to send an integer from each goroutine call
 	for i := 0; i < 5; i++ {
+		// When "sleepyGopher()" hits 
 		gopherId := <-c
 		fmt.Printf("gopher %v has finished sleeping\n", gopherId)
+	}
+}
+
+// code from listing 30.5 describes use of select statements, which run when a
+// value is received in one of the channels in a select case. It also describes
+// use of the time.After method to set a timeout.
+func verThree() {
+	sleepyGopher := func(id int, c chan int) {
+		time.Sleep(time.Duration(rand.Intn(4000)) * time.Millisecond)
+		c <- id
+	}
+
+	c := make(chan int)
+	timeout := time.After(3 * time.Second)
+
+	fmt.Println("Running code from listing 30.5")
+
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i, c)
+	}
+	for i := 0; i < 5; i++ {
+		select {
+		case gopherId := <-c:
+			fmt.Printf("gopher %v has finished sleeping\n", gopherId)
+		case <-timeout:
+			fmt.Println("my patience has ran out")
+			return
+		}
 	}
 }
