620d927a5be6cb40b8764b0f93e11172c933c023
coatsd <35239532+coatsd@users.noreply.github.com>
2020-11-03 12:15:04 -0800

    Completed the c30 exercise remove-Identical

--- c30_goroutines/remove_identical.go
+++ c30_goroutines/remove_identical.go
@@ -4,6 +4,46 @@ import (
 	"fmt"
 )
 
+func source(ds chan string) {
+	for _, v := range []string{"value 1", "value 1", "value 2", "value 3"} {
+		ds <- v
+	}
+	close(ds)
+}
+
+func filter(us, ds chan string) {
+	var prev string
+	for {
+		v, ok := <-us
+		if !ok {
+			break
+		}
+		if v != prev {
+			ds <- v
+		}
+		prev = v
+	}
+	close(ds)
+}
+
+func printString(us chan string) {
+	for {
+		v, ok := <-us
+		if !ok {
+			return
+		}
+		fmt.Println(v)
+	}
+}
+
+func runCompose() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go source(c1)
+	go filter(c1,c2)
+	printString(c2)
+}
+
 func main() {
-	
+	runCompose()
 }
