477acfe504733c4c882cab22fcdc0010ddf41748
coatsd <35239532+coatsd@users.noreply.github.com>
2020-11-18 16:00:31 -0800

    simplified some of the code in c31

--- c31_mutexes/rover.go
+++ c31_mutexes/rover.go
@@ -9,10 +9,10 @@ import (
 type command int
 
 const (
-	stop = command(0)
-	start = command(1)
-	left = command(2)
-	right = command(3)
+	stop command = iota
+	start
+	left
+	right
 )
 
 type RoverDriver struct {
@@ -20,39 +20,37 @@ type RoverDriver struct {
 }
 
 func NewRoverDriver() *RoverDriver {
-	r := &RoverDriver{c: make(chan command),}
+	r := &RoverDriver{c: make(chan command)}
 	go r.drive()
 	return r
 }
 
 func (r *RoverDriver) drive() {
-	pos := image.Point{X: 0, Y: 0,}
-	dir := image.Point{X: 0, Y: 0,}
-	dirState := image.Point{X: 1, Y: 0,}
+	pos := image.Point{X: 0, Y: 0}
+	dir := image.Point{X: 0, Y: 0}
+	dirState := image.Point{X: 1, Y: 0}
 	updateInterval := 500 * time.Millisecond
-	nextMove := time.After(updateInterval)
 	for {
 		select {
 		case c := <-r.c:
 			switch c {
 			case stop:
 				dirState = dir
-				dir = image.Point{X: 0, Y: 0,}
+				dir = image.Point{X: 0, Y: 0}
 				fmt.Println("Rover stopped")
 			case start:
 				dir = dirState
 				fmt.Println("Rover started")
 			case right:
-				dir = image.Point{X: -dir.Y, Y: dir.X,}
+				dir = image.Point{X: -dir.Y, Y: dir.X}
 				fmt.Println("turned right") 
 			case left:
-				dir = image.Point{X: dir.Y, Y: -dir.X,}
+				dir = image.Point{X: dir.Y, Y: -dir.X}
 				fmt.Println("turned left")
 			}
-		case <-nextMove:
+		case <-time.After(updateInterval):
 			pos = pos.Add(dir)
 			fmt.Printf("moved to %v\n", pos)
-			nextMove = time.After(updateInterval)
 		}
 	}
 }
--- c31_mutexes/scrape.go
+++ c31_mutexes/scrape.go
@@ -9,6 +9,9 @@ type Visited struct {
 	mu sync.Mutex
 	visited map[string]int
 }
+func makeVisited() Visited {
+	return Visited{visited: make(map[string]int)}
+}
 
 func (v *Visited) VisitLink(url string) int {
 	v.mu.Lock()
@@ -16,9 +19,21 @@ func (v *Visited) VisitLink(url string) int {
 	c := v.visited[url]
 	c++
 	v.visited[url] = c
+	fmt.Println(url)
 	return c
 }
 
 func main() {
-	
+	v := makeVisited()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		v.VisitLink("https://www.facebook.com")
+	}()
+	go func() {
+		defer wg.Done()
+		v.VisitLink("https://www.google.com")
+	}()
+	wg.Wait()
 }
